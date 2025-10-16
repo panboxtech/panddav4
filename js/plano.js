1a8297df0cd55107ed35c3fff65eabb05de895bc
panboxtech <panboxtech@gmail.com>
2025-10-16 13:36:58 -0300

    Delete js/plano.js

--- js/plano.js
+++
@@ -1,7 +0,0 @@
-// Plano entity logic
-window.PlanoService = {
-  async list(){ return MockDB.getAll('planos'); },
-  async create(payload){ return MockDB.insert('planos', payload); },
-  async update(id, patch){ return MockDB.update('planos', id, patch); },
-  async remove(id){ return MockDB.remove('planos', id); }
-};
